f8c7d4d08b3eb64e0735109f1570268e1635706f
Valerie Kolling <valerie.kolling@gmx.de>
2021-05-22 13:37:26 +0200

    created seed file and added user

---
+++ seeders/20210522113106-seeds.js
@@ -0,0 +1,19 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    return queryInterface.bulkInsert('Users', [{
+      email: 'mailatvalerie@gmail.com',
+      name: 'Valerie',
+      password: 'secret',
+      createdAt: new Date(),
+      updatedAt: new Date()
+     }], {});
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    return queryInterface.bulkDelete('Users', [{
+      email :'mailatvalerie@gmail.com'
+    }])
+  }
+};
