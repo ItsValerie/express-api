3437b886af60add7bd8ffb0d7fae0163ca61b934
Valerie Kolling <valerie.kolling@gmx.de>
2021-05-22 17:09:40 +0200

    Added further associations and env variables

--- models/manufacturer.js
+++ models/manufacturer.js
@@ -10,6 +10,7 @@ module.exports = (sequelize, DataTypes) => {
      * The `models/index` file will call this method automatically.
      */
     static associate(models) {
+      Manufacturer.hasMany(models.Phone), { onDelete: 'CASCADE' };
       // define association here
     }
   };
