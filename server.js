fce7083d5fa53b8871dd62e83bd10968a940de79
Valerie Kolling <valerie.kolling@gmx.de>
2021-05-22 23:59:32 +0200

    Added routes for GET manufacturer(s)

--- server.js
+++ server.js
@@ -1,29 +1,82 @@
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
+const jsonParser = bodyParser.json()
 
 const db = require('./models');
+
+// DO i need this?
+// app.use(express.urlencoded({ extended: true }));
+// app.use(express.json());
+
 
 app.listen(3000, function() {
   db.sequelize.sync();
 });
 
 
+
+// ROUTES FOR MANUFACTURERS ENDPOINTS
 const Manufacturer = db.Manufacturer;
 
-app.post('/manufacturers', function (req, res) {
-  Manufacturer.create({
+// POST
+app.post('/manufacturers', jsonParser, function (req, res, next) {
+  const manufacturer = Manufacturer.create({
     name: req.body.name,
     location: req.body.location
   })
-  .then (function (manufacturer) {
-    res.json(manufacturer);
-  });
+  if (!manufacturer) {
+    return next(
+      new Error(`Unsuccessful`, 404) // does this work?
+    );
+  };
+  res.status(200).json({ success: true, data: req.body });
 });
 
-
+// GET
 app.get('/manufacturers', function(req, res) {
   Manufacturer.findAll()
     .then(function (manufacturers) {
       res.json(manufacturers);
     });
 });
+
+
+// GET !!! handle error -not found-
+app.get('/manufacturers/:id', function(req, res) {
+  const manufacturer = Manufacturer.findAll({
+    where: {
+      id: req.params.id
+    }
+  })
+  .then(function (manufacturer) {
+       if (!manufacturer) {
+            return 'Manufacturer not found';
+        }
+        return res.json(manufacturer);
+    });
+});
+
+// DELETE manufacturers
+app.delete('manufacturers/:id', function(req, res) {
+   Manufacturer.destroy({
+    where: {
+      id: req.params.id
+    }
+  })
+  return res.status(200).json({ success: true, message: "Deleted" });
+});
+
+
+// PUT manufacturers
+app.patch('manufacturers/:id', jsonParser, function(req, res) {
+  Manufacturer.update({
+    name: req.body.name,
+    location: req.body.location}, {
+      where: {
+      id: req.params.id
+    }
+   })
+  return res.status(200).json({ success: true, data: req.body });
+});
+
