d22696a1a69330b9b4d7ff056dee96d03d147431
Valerie Kolling <valerie.kolling@gmx.de>
2021-05-25 11:31:05 +0200

    Added authorization for Manufacturer routes

--- routes/manufacturer.js
+++ routes/manufacturer.js
@@ -5,10 +5,10 @@ const passport = require('passport');
 const jwt = require('jsonwebtoken');
 
 router.get('/manufacturers', manufacturer.getManufacturers);
-router.get('/manufacturer/:id', manufacturer.getManufacturer);
+router.get('/manufacturer/:id', passport.authenticate('jwt', {session: false}), manufacturer.getManufacturer);
 router.get('/manufacturer/:id/phones', manufacturer.getManufacturerPhones);
 router.post('/manufacturer/create', passport.authenticate('jwt', {session: false}), manufacturer.createManufacturer);
-router.put('/manufacturer/:id', manufacturer.updateManufacturer);
-router.delete('/manufacturer/:id', manufacturer.deleteManufacturer);
+router.put('/manufacturer/:id', passport.authenticate('jwt', {session: false}), manufacturer.updateManufacturer);
+router.delete('/manufacturer/:id', passport.authenticate('jwt', {session: false}), manufacturer.deleteManufacturer);
 
 module.exports = router;
