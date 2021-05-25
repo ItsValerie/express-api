b0529f3f6dd374bc59d3e4c2ddaf397c20a261dc
Valerie Kolling <valerie.kolling@gmx.de>
2021-05-25 11:37:38 +0200

    Styling edits

--- routes/user.js
+++ routes/user.js
@@ -6,9 +6,3 @@ router.post('/user/register', user.registerUser);
 router.post('/user/login', user.loginUser);
 
 module.exports = router;
-
-
-// - POST `/user/register`; body: `{email, name, password}`; authorisation: `false`
-//     - Should handle user registration; password has to be hashed in database;
-// - POST `/user/login`; body: `{email, password}`; authorisation: `false`
-//     - Should handle user login and if successful, return Basic Authorisation token;
