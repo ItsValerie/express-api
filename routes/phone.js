c816fb266265d4eef935e336a5d8ae1574107e9e
Valerie Kolling <valerie.kolling@gmx.de>
2021-05-25 11:36:53 +0200

    Added authorization for Phone routes

--- routes/phone.js
+++ routes/phone.js
@@ -1,20 +1,12 @@
 const express = require('express');
 const router = express.Router();
 const phone = require('../controllers/phone.controller');
+const passport = require('passport');
+const jwt = require('jsonwebtoken');
 
 router.get('/phone/:id', phone.getPhone);
-router.post('/phone/create/:manufacturer_id', phone.createPhone);
-router.put('/phone/:id', phone.updatePhone);
-router.delete('/phone/:id', phone.deletePhone);
+router.post('/phone/create/:manufacturer_id', passport.authenticate('jwt', {session: false}), phone.createPhone);
+router.put('/phone/:id', passport.authenticate('jwt', {session: false}), phone.updatePhone);
+router.delete('/phone/:id', passport.authenticate('jwt', {session: false}), phone.deletePhone);
 
 module.exports = router;
-
-
-// - POST `/phone/create/{manufacturer_key}`; body: `{name, quantity, releaseDate}`; authorisation: `true`
-//     - Should create new phone object that is assigned to designated manufacturer
-// - PUT `/phone/{phone_key}`; body: `{name, quantity, releaseDate}`; authorisation: `true`
-//     - Should update data about specific phone model;
-// - GET `/phone/{phone_key}`; authorisation: `false`
-//     - Should return only data about specified phone;
-// - DELETE `/phone/{phone_key}`; authorisation: `true`
-//     - Should remove phone;
